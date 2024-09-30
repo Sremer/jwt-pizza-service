0f8a5b82d4503ac76f127a2bb81cc8927e16ae23
Seth Remer <seth.e.remer@gmail.com>
2024-09-30 16:43:29 -0600

    #d3 add(initial logout and update user tests)

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,3 +1,32 @@
-test('hello world', () => {
-  expect('hello' + ' world').toBe('hello world');
+const request = require('supertest');
+const app = require('../service');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+let userId;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  userId = registerRes.body.user.id;
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  expect(loginRes.body.user).toMatchObject(user);
+});
+
+test('logout', async () => {
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`).send();
+  expect(logoutRes.status).toBe(200);
+});
+
+test('update user', async () => {
+  const updateUserRes = await request(app).put('/api/auth/' + userId).set('Authorization', `Bearer ${testUserAuthToken}`).send(testUser);
+  expect(updateUserRes.status).toBe(200);
 });
